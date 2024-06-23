ec30172f393788621332ff8be91347524b84de33
Bruno Mioto <brunomioto97@gmail.com>
2024-06-23 20:03:59 -0300

    partially implements tests with mocking

--- tests/testthat/test-read_feow.R
+++ tests/testthat/test-read_feow.R
@@ -3,10 +3,21 @@ test_that("Internet connection is available", {
 })
 
 test_that("Function read_feow works correctly with internet", {
+
   skip_if_not(RCurl::url.exists("https://github.com/brunomioto/feowR"),
               message = "No internet connection to test read_feow function")
 
-  result <- read_feow(overwrite = TRUE)
+  result <- read_feow()
   expect_s3_class(result, "sf")
   expect_true(nrow(result) > 0)
 })
+
+# url.exists <- NULL
+# test_that("Function read_feow fails without internet", {
+#   local_mocked_bindings(
+#     # url.exists <- NULL
+#     url.exists = function(url) FALSE
+#   )
+#    expect_error(read_feow())
+#
+# })
